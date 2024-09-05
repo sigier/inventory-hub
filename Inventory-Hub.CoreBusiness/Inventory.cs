07d3d7180557e55ecc55e4bfd264df275b76c02c
sigier <jotulw@proton.me>
2024-09-05 10:19:26 +0300

    feat.: Cancel button added to AddInventory

--- Inventory-Hub.CoreBusiness/Inventory.cs
+++ Inventory-Hub.CoreBusiness/Inventory.cs
@@ -1,10 +1,20 @@
-﻿namespace Inventory_Hub.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Hub.CoreBusiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
+
+        [Required]
+        [StringLength(150)]
         public string InventoryName { get; set; } = string.Empty;
+
+        [Range(0, Int32.MaxValue, ErrorMessage = "Please, enter correct value, greater or equal to zero.")]
         public int Quantity { get; set; }
+
+        [Range(0, Int32.MaxValue, ErrorMessage = "Please, enter correct value, greater or equal to zero.")]
+
         public double Price { get; set; }
     }
 }
