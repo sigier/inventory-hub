5f49c3da61bd888733993ab535c88b926b5507bc
sigier <jotulw@proton.me>
2024-09-19 18:03:47 +0300

    feat.: ProduceProduct added

---
+++ IMS.WebApp/ViewModels/ProduceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using IMS.CoreBusiness;
+using System.ComponentModel.DataAnnotations;
+using IMS.WebApp.ViewModelValidations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class ProduceViewModel
+    {
+        [Required]
+        public string ProductionNumber { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "You have to select a product.")]
+        public int ProductId { get; set; }
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater or equal to 1.")]
+        [Produce_EnsureEnoughInventoryQuantity]
+        public int QuantityToProduce { get; set; }
+
+        public Product? Product { get; set; }
+    }
+}
